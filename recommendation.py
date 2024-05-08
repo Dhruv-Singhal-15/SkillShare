ba45333b856fa00f937a4796e2f76789adca7db2
prajwal26dec02 <99467687+prajwal26dec02@users.noreply.github.com>
2024-05-08 20:41:46 +0530

    Add files via upload

--- recommendation.py
+++ recommendation.py
@@ -19,8 +19,9 @@ def load():
     num_rating_df=rating_with_name.groupby('Course_Title').count()['Rating'].reset_index()
     num_rating_df.rename(columns={'Rating':'num_ratings'},inplace=True)
 
-    avg_rating_df=rating_with_name.groupby('Course_Title')['Rating'].mean().reset_index()
-    avg_rating_df.rename(columns={'Rating':'avg_ratings'},inplace=True)
+    avg_rating_df = rating_with_name.groupby('Course_Title')['Rating'].mean().reset_index()
+    avg_rating_df['avg_ratings'] = avg_rating_df['Rating'].round(2)
+    avg_rating_df.drop(columns=['Rating'], inplace=True)
 
     popular_df = num_rating_df.merge(avg_rating_df,on='Course_Title')
     popular_df=popular_df.merge(courses_df,on='Course_Title',how='right').fillna(0)
@@ -31,16 +32,15 @@ def load():
     pickle.dump(popular_df,open(file_path,'wb'))
     
 def recommend(skill,course_name):
-    courses=pd.read_csv('C:\\Users\\Rushil\\Desktop\\Dataset\\Courses.csv')
-    ratings_df=pd.read_csv('C:\\Users\\Rushil\\Desktop\\Dataset\\Rating.csv')
+    courses=Course.query.all()
+    courses_data=[{'cid': course.cid,'Course_Title': course.title,'Course_by': course.by,'Subject': course.subject,'Link': course.link,'Image_link': course.img_link} for course in courses]
+    courses_df=pd.DataFrame(courses_data)
 
     ratings=Rating.query.all()
-    if ratings:
-        ratings_data=[{'id': rating.user_id,'cid': rating.cid,'Rating': rating.rating} for rating in ratings]
-        ratings_df_db=pd.DataFrame(ratings_data)
-        ratings_df=pd.concat([ratings_df,ratings_df_db],ignore_index=True)
+    ratings_data=[{'id': rating.user_id,'cid': rating.cid,'Rating': rating.rating} for rating in ratings]
+    ratings_df=pd.DataFrame(ratings_data)
     
-    rating_with_name=ratings_df.merge(courses,on='cid')
+    rating_with_name=ratings_df.merge(courses_df,on='cid')
     
     pt=rating_with_name.pivot_table(index='Course_Title',columns='id',values='Rating')
     pt.fillna(0,inplace=True)
@@ -53,13 +53,13 @@ def recommend(skill,course_name):
     
     
     index = np.where(pt.index == course_name.values[0])[0][0]
-    sub_of_course = courses[courses['Course_Title'] == course_name.values[0]]['Subject'].values[0]
+    sub_of_course = courses_df[courses_df['Course_Title'] == course_name.values[0]]['Subject'].values[0]
     tolearn_index = path_db[path_db['skill'] == skill].index[0]
     values = path_db.iloc[tolearn_index, 1:].tolist()
     next_step_index = values.index(sub_of_course) + 1
     if next_step_index >= len(path_db.columns) - 1:
         return "/dashboard", "Congratulations"
-    next_step = path_db[path_db['skill'] == skill].iloc[tolearn_index, next_step_index+1]
+    next_step = path_db[path_db['skill'] == skill].iloc[0, next_step_index+1]
     sim_items = sorted(list(enumerate(sim_score[index])), key=lambda x: x[1], reverse=True)[1:]
     recommended_courses=[]
     count=0
@@ -68,7 +68,7 @@ def recommend(skill,course_name):
             break
         course_title = pt.index[i[0]]
         similarity_score = i[1]
-        if courses[courses['Course_Title'] == course_title]['Subject'].values[0] == next_step:
+        if courses_df[courses_df['Course_Title'] == course_title]['Subject'].values[0] == next_step:
             recommended_courses.append((course_title, similarity_score))
             count += 1
     recommended_courses.sort(key=lambda x: x[1], reverse=True)
